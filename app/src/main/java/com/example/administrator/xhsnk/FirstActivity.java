c99cc15e5af9df1a7955523bfa153328dda22eee
student <student@hubu.edu.cn>
2019-04-29 17:33:24 +0800

    bi

---
+++ app/src/main/java/com/example/administrator/xhsnk/FirstActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.xhsnk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+
+    }
+
+   button1.
+
+
+}
